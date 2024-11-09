bb681d105b7ca0b56bf0b8eef43533317013867b
MilankaHuliganka <lizochka.canfetka@gmail.com>
2024-11-09 16:52:38 +0300

    lizochka-canfetka

---
+++ lesson.object/script.js
@@ -0,0 +1,28 @@
+//1
+// let user =  {
+//     name:"John",
+//     surname:"Smith",
+// }
+// user.name=("Pete")
+// delete user.name
+
+//2 
+function isEmpty(object) {
+    for(key in obj) {
+        return false;{
+        return true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//3
+
+ 
